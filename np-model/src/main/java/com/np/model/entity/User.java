016d165ecace657f1b40aabec70156e7a9bb8ba7
Amit Kumar <amit.kumar@kritikalsolutions.com>
2015-07-26 15:28:58 +0530

    Model

---
+++ np-model/src/main/java/com/np/model/entity/User.java
@@ -0,0 +1,66 @@
+package com.np.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class User {
+
+    private String Id;
+	private String username;
+	private String password;
+	private boolean enabled;
+ 
+	public User() {
+	}
+ 
+	public User(String username, String password, boolean enabled) {
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Id", unique = true, 
+		nullable = false, length = 45)
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	@Id
+	@Column(name = "username", unique = true, 
+		nullable = false, length = 45)
+	public String getUsername() {
+		return this.username;
+	}
+ 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+ 
+	@Column(name = "password", 
+		nullable = false, length = 60)
+	public String getPassword() {
+		return this.password;
+	}
+ 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+ 
+	@Column(name = "enabled", nullable = false)
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+ 
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+ 
+}
